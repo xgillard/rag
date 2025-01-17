dcb628c81ed7f4d0bfd1407ca00d0b267a6be7fc
Xavier GILLARD <xavier.gillard@uclouvain.be>
2025-01-17 16:24:07 +0100

    qq xp
    - la réponse est streaming (du moins avec le modele ort)
    - les réponses sont meilleures avec le modele hf (non quantisé)

--- uclouvain/ask_question.py
+++ uclouvain/ask_question.py
@@ -2,36 +2,40 @@
 An example script to ask a question and retrieve documents in the corpus.
 """
 
-from sys import stderr
-
+from argparse import ArgumentParser
 from requests import post
 
-
-def ask_question(host_port: str, question: str) -> None:
+def ask_question(host_port: str, nb_search: int, nb_tokens: int, sources: bool, question: str) -> None:
     """Asks a question about the corpus."""
     response = post(
         f"http://{host_port}/rag",
-        params={
+        json={
             "question": question,
+            "search_results": nb_search,
+            "generation_tokens": nb_tokens,
+            "append_sources": sources,
         },
+        stream=True,
     )
-    if response.status_code == 200:
-        print(response.text)
-    else:
-        print("!" * 80)
-        print(response.text, file=stderr)
-        print("!" * 80)
+    for txt in response.iter_content(decode_unicode=True):
+        if txt:
+            print(txt, end="", flush=True)
 
-
-def main(host_port: str) -> None:
+def main(host_port: str, nb_search: int, nb_tokens: int, sources: bool) -> None:
     """
     Main function: reads a question from stdin and
     search for answers in the corpus.
     """
     question: str = input("Your Question: ")
-    ask_question(host_port, question)
+    ask_question(host_port, nb_search, nb_tokens, sources, question)
 
 
 if __name__ == "__main__":
-    main("localhost:8000")
+    parser = ArgumentParser("ask question: une petite démo de RAG à l'UCL")
+    parser.add_argument("-n", '--nb-search', default=5,   help="Nombre de résultats lors de la phase de recherche")
+    parser.add_argument("-t", "--nb-tokens", default=128, help="Nombre max de tokens générés pour la réponse")
+    parser.add_argument("-s", "--sources", default=False, help="Afficher le détail des sources apres la réponse ?")
+
+    args = parser.parse_args()
+    main("localhost:8000", args.nb_search, args.nb_tokens, args.sources)
     #main("automemo.sipr.ucl.ac.be:8000")
--- uclouvain/backend/src/backend.py
+++ uclouvain/backend/src/backend.py
@@ -6,20 +6,24 @@ developer willing to conduct semantic search & indexation.
 
 from __future__ import annotations
 
-import unicodedata
-from typing import TYPE_CHECKING, NamedTuple
+from typing import TYPE_CHECKING, Callable, NamedTuple
 
+import onnxruntime_genai as og
 import pymupdf
 import pymupdf4llm
 import torch
 from fastapi import FastAPI, HTTPException, UploadFile
+from fastapi.responses import StreamingResponse
+from pydantic import BaseModel
 from sentence_transformers import SentenceTransformer
-from transformers import pipeline
+from transformers import AutoModelForCausalLM, AutoTokenizer
 
 from .database import RelevantDocument, find_similar_to, is_already_indexed, save_chunks
 from .preprocessing import preprocess
 
 if TYPE_CHECKING:
+    from collections.abc import Iterator
+
     import numpy as np
 
 app = FastAPI()
@@ -27,7 +31,70 @@ app = FastAPI()
 ###### LANGUAGE MODELS ########################################################
 DEVICE: torch.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 EMBED: SentenceTransformer = SentenceTransformer("intfloat/multilingual-e5-large-instruct", device=DEVICE)
-GEN  = pipeline("text-generation", model="meta-llama/Llama-3.2-1B-Instruct", device_map="auto")
+#GEN  = pipeline("text-generation", model="meta-llama/Llama-3.2-1B-Instruct", device_map="auto")
+
+def ort_llm_model() -> og.Model:
+    """Return the appropriate ORT-genai model."""
+    path: str = "onnx/llama-3.2-1b-instruct__int4"
+    if torch.cuda.is_available():
+        path  = "onnx/llama-3.2-1b-instruct__int4"
+    return og.Model(path)
+
+def ort_llm_respond(model: og.Model, user_prompt: str, new_tokens: int) -> Iterator[str]:
+    """Return the text response from the llm using a quantized llm."""
+    tokenizer: og.Tokenizer = og.Tokenizer(model)
+    streamer: og.TokenizerStream = tokenizer.create_stream()
+    prompt: str = f"<|user|>\n{user_prompt} <|end|>\n<|assistant|>## Answer\n"
+    tokens: list[int] = tokenizer.encode(prompt)
+    # generation config
+    params: og.GeneratorParams = og.GeneratorParams(model)
+    params.set_search_options(
+        max_length = len(tokens) + new_tokens,
+        temperature = 0.05,
+        top_k = 5,
+        top_p = 0.95,
+        repetition_penalty = 1.1,
+    )
+    params.input_ids = tokens
+    generator: og.Generator = og.Generator(model, params)
+    # generation loop
+    while not generator.is_done():
+        generator.compute_logits()
+        generator.generate_next_token()
+        yield streamer.decode(generator.get_next_tokens()[0])
+
+def ort_pipeline() -> Callable[[str], Iterator[str]]:
+    """Pipeline to generate text response to a prompt."""
+    model = ort_llm_model()
+    return lambda text, new_tokens: ort_llm_respond(model, text, new_tokens)
+
+def hf_llm_model():
+    model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-3.2-1B-Instruct")
+    if torch.cuda.is_available():
+        model = model.to('cuda')
+    return model
+
+def hf_llm_respond(model, user_prompt, new_tokens):
+    tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-3.2-1B-Instruct")
+    prompt = f"<|user|>\n{user_prompt} <|end|>\n<|assistant|>## Answer\n"
+    tokens = tokenizer(prompt, return_tensors='pt').to(model.device)
+    outputs = model.generate(
+        **tokens,
+        max_new_tokens=new_tokens,
+        do_sample=True,
+        temperature=0.05,
+        top_k=5,
+        top_p=0.95,
+    )
+    output = tokenizer.batch_decode(outputs, skip_special_tokens=True)[0]
+    yield output[len(prompt)-1:]
+
+def hf_pipeline() -> Callable[[str], Iterator[str]]:
+    """Pipeline to generate text response to a prompt."""
+    model = hf_llm_model()
+    return lambda text, new_tokens: hf_llm_respond(model, text, new_tokens)
+
+GEN = hf_pipeline()
 
 ###### UTILITY CLASSES ########################################################
 class EmbeddedChunk(NamedTuple):
@@ -35,6 +102,26 @@ class EmbeddedChunk(NamedTuple):
 
     text: str
     embedding: np.ndarray
+
+class SearchRequest(BaseModel):
+    """An user request expressed in natural language to retrieve user documents."""
+
+    question: str
+    search_results: int = 5
+
+class GenerationRequest(BaseModel):
+    """A request to generate a plaintext response for a given prompt."""
+
+    prompt: str
+    generation_tokens: int = 256
+
+class RagRequest(BaseModel):
+    """An user request asked to the system using natural language."""
+
+    question: str
+    search_results: int = 5
+    generation_tokens: int = 256
+    append_sources: bool = True
 
 
 ###### SERVICE ENDPOINTS ######################################################
@@ -80,45 +167,55 @@ async def index_file(path_to_doc: str, file: UploadFile) -> str | None:
 
 
 @app.post("/retrieval/")
-async def retrieve(user_request: str, nb_results: int = 5) -> list[RelevantDocument]:
+async def retrieve(request: SearchRequest) -> list[RelevantDocument]:
     """Retrieve the `nb_results` most relevant documents based on a user request."""
     prompt: str = f"""Given a user request, attempt to find the most relevant
     piece of information in order to satisfy that user's request.
 
     # User Request:
-    {user_request}
+    {request.question}
     """
     # just drop the leading whitespaces in the prompt.
     prompt: str = "\n".join(line.strip() for line in prompt.splitlines())
-    return find_similar_to(EMBED.encode(prompt), nb_results)
+    return find_similar_to(EMBED.encode(prompt), request.search_results)
 
 @app.post("/rag")
-async def rag(question: str) -> str:
+async def rag(req: RagRequest) -> StreamingResponse:
     """Perform a complete rag pipeline: it starts from a user requests and generates an answer."""
-    documents: list[RelevantDocument] = await retrieve(question, nb_results=5)
+    documents: list[RelevantDocument] = await retrieve(SearchRequest(
+        question=req.question,
+        search_results=req.search_results,
+    ))
     sources: str = "\n".join(
-        f"{'#'*40}\n{d.path_to_doc}\n{'-'*40}\n{d.text}" for d in documents
+        f"Ref: [{i}]\nDocument: **{d.path_to_doc}**\n```\n{d.text}```" for i,d in enumerate(documents)
     )
     prompt: str = f"""
-    You are an assistant to the university archivists. Reply to the archivist's
-    question as good as possible using only the (possibly relevant) pieces of archive
-    below. When an answer cannot be found in the given archives,
-    just say "SORRY, I COULD NOT FIND THE ANSWER IN THE PROVIDED ACHIVES".
-    Do not forget to provide links to the sources you've used at the end of your answer.
+    Use information from the given documents to answer the question below.
+    Be succinct in your response.
+    When unsure about the answer, express your doubts clearly.
+
+    ## Question
+    {req.question}
 
     ## Archives
     {sources}
-
-    ## Archivist's Question
-    {question}
     """
     prompt: str = "\n".join(line.strip() for line in prompt.splitlines())
-    return await generate(prompt)
+
+    def _respond() -> Iterator[str]:
+        """Generate response chunk by chunk."""
+        yield from GEN(prompt, req.generation_tokens)
+        if req.append_sources:
+            yield "\n\n\n\n"
+            yield "## Sources\n"
+            yield sources
+
+    return StreamingResponse(_respond(), media_type="text/plain")
 
 @app.post("/generation/")
-async def generate(prompt: str) -> str:
+async def generate(request: GenerationRequest) -> StreamingResponse:
     """Generate a text response to the given prompt."""
-    return GEN([{"role": "user", "content": prompt}], max_new_tokens=512)[0]["generated_text"][-1]["content"]
+    return StreamingResponse(GEN(request.prompt, request.generation_tokens), media_type="text/plain")#GEN([{"role": "user", "content": prompt}], max_new_tokens=512)[0]["generated_text"][-1]["content"]
 
 
 async def get_md_text(file: UploadFile) -> str:
